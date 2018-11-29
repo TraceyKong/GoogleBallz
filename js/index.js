57e00734ca9e0dbc63eb63b2d985036dd9ba4f4e
TraceyKong <31457793+TraceyKong@users.noreply.github.com>
2018-11-29 18:33:31 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-import Ball from "ball.js";
+//import Ball from "ball.js";
 
 let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
 
-var ball = new Ball();
+//var ball = new Ball();
 
-ball.draw(ctx);
+//ball.draw(ctx);
