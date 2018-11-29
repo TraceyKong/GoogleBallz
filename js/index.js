dc19ee16d0e2f5c802a39ef08431973c5420ca7a
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2018-11-29 18:29:47 -0500

    import error

--- js/index.js
+++ js/index.js
@@ -1,23 +1,8 @@
+import Ball from "ball.js";
+
 let canvas = document.getElementById("gameScreen");
 
 let ctx = canvas.getContext("2d");
-
-class Ball{
-
-    constructor(){
-        this.image = document.getElementById("ball");
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image,
-            canvas.width/2 - this.image.width/2,
-            canvas.height - this.image.height, 21, 8);
-    }
-
-    update(){
-
-    }
-}
 
 var ball = new Ball();
 
